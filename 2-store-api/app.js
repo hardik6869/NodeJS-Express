35abecbf38717e31b42a0872c9a99b678e1d300b
hardik6869 <sorthiyahardik6869@gmail.com>
2023-04-13 11:18:43 +0530

    02-Setup: NodeJs, MongoDB & Express Environment

--- 2-store-api/app.js
+++ 2-store-api/app.js
@@ -1,1 +1,42 @@
-console.log("02 Store API");
+require("dotenv").config();
+require("colors");
+// Async Errors
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const ErrorMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send(
+    '<h1> Store API </h1> <a href="/api/v1/products"> Products Routes </a>'
+  );
+});
+
+// Product Route
+
+app.use(notFoundMiddleware);
+app.use(ErrorMiddleware);
+
+const port = process.env.PORT || 5001;
+
+const start = async () => {
+  try {
+    // connectDB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(
+      port,
+      console.log(`Server is Listening port ${port}...`.bgGreen.underline.bold)
+    );
+  } catch (error) {
+    console.log(`${error}`.bgRed.underline.bold);
+  }
+};
+
+start();
