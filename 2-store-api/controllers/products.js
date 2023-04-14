8890414df603dc1a376fea0a7f1c6abfa9f1cce1
hardik6869 <sorthiyahardik6869@gmail.com>
2023-04-14 11:01:26 +0530

    02-Sorting & Searching Functionality

--- 2-store-api/controllers/products.js
+++ 2-store-api/controllers/products.js
@@ -1,9 +1,35 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "Products Testing Routes" });
+  const search = "a";
+  const products = await Product.find({
+    $regex: search,
+    $options: "i",
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products Routes" });
+  const { featured, company, name, sort } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
