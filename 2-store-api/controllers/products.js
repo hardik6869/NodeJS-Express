c2b9dc0200db165631d42d69c4f29c14f477280c
hardik6869 <sorthiyahardik6869@gmail.com>
2023-04-14 11:34:46 +0530

    02-Select Functionality

--- 2-store-api/controllers/products.js
+++ 2-store-api/controllers/products.js
@@ -2,31 +2,37 @@ const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   const search = "a";
-  const products = await Product.find({
-    $regex: search,
-    $options: "i",
-  });
+  const products = await Product.find({}).sort("name").select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
+
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
+
   if (company) {
     queryObject.company = company;
   }
+
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
+
   let result = Product.find(queryObject);
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
   }
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
